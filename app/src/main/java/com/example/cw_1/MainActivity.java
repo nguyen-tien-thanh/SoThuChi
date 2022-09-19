b7fe502efd7cb5de65ad27b5c08171b790824e53
Tien Thanh <nguyentienthanh.tgdd@gmail.com>
2022-09-19 08:33:51 +0700

    Add date picker

--- app/src/main/java/com/example/cw_1/MainActivity.java
+++ app/src/main/java/com/example/cw_1/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.cw_1;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -22,10 +25,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Button dateButton;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +39,16 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initDatePicker();
+        dateButton = findViewById(R.id.datePicker);
+        dateButton.setText(getTodayDate());
 
-        TextView id = (TextView)findViewById(R.id.editId);
-        TextView date = (TextView)findViewById(R.id.editDate);
-        TextView note = (TextView)findViewById(R.id.editNote);
-        TextView money = (TextView)findViewById(R.id.editMoney);
-        TextView category = (TextView)findViewById(R.id.editCategory);
-        Button btnSave = (Button)findViewById(R.id.btnSave);
+        TextView id = findViewById(R.id.editId);
+        TextView date = findViewById(R.id.editDate);
+        TextView note = findViewById(R.id.editNote);
+        TextView money = findViewById(R.id.editMoney);
+        TextView category = findViewById(R.id.editCategory);
+        Button btnSave = findViewById(R.id.btnSave);
 
 
         btnSave.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +81,83 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    private String getTodayDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+    }
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if (month == 1){
+            return "JAN";
+        }
+        if (month == 2){
+            return "FEB";
+        }
+        if (month == 3){
+            return "MAR";
+        }
+        if (month == 4){
+            return "APR";
+        }
+        if (month == 5){
+            return "MAY";
+        }
+        if (month == 6){
+            return "JUN";
+        }
+        if (month == 7){
+            return "JUL";
+        }
+        if (month == 8){
+            return "AUG";
+        }
+        if (month == 9){
+            return "SEP";
+        }
+        if (month == 10){
+            return "OCT";
+        }
+        if (month == 11){
+            return "NOV";
+        }
+        if (month == 12){
+            return "DEC";
+        }
+        return "JAN";
+    }
+
+
+    public void openDatePicker(View view){
+        datePickerDialog.show();
     }
 
     @SuppressLint("NewApi")
